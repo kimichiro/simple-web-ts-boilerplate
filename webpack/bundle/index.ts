9fc7fdeef059e0d4b22f382f567ced3ba34a253c
Panupong Pasitanon <panupong.pasitanon@agoda.com>
2020-01-14 18:24:14 +0700

    add ignore and rename files

--- webpack/bundle/index.ts
+++ webpack/bundle/index.ts
@@ -1,8 +1,8 @@
 import { html, ts, tsx } from './functions'
 
 export const clientBundles = [
-    tsx({ name: 'main', path: 'client/main' }),
-    html({ name: 'main', path: 'client/main' }),
+    tsx({ name: 'main', path: 'client/pages' }),
+    html({ name: 'main', path: 'client/pages' }),
 ]
 
 export const serverBundles = [
